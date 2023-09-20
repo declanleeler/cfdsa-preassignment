f2bc1aa8d73677a1cbdcb4ad22af253774939038
declanleeler <declanlee-ler@hotmail.com>
2023-09-21 00:11:28 +0800

    Add app and view

---
+++ workshop01/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const quotes = [
+  "Logic will get you from A to B. Imagination will take you everywhere.",
+  "There are 10 kinds of people. Those who know binary and those who don't.",
+  "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.",
+  "It's not that I'm so smart, it's just that I stay with problems longer.",
+  "It is pitch dark. You are likely to be eaten by a grue.",
+];
+
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  const quote = quotes[Math.floor(Math.random() * 5)];
+
+  res.render("index", { quote: quote });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
